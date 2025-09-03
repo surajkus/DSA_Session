d5a4215fe858d775731546aa8bf577f45b09b237
surajkus <kushwahasuraj752@gmail.com>
2025-09-03 10:38:56 +0530

    day 4

---
+++ missElementinArrays.java
@@ -0,0 +1,16 @@
+public class missElementinArrays {
+    public static void main(String args[]){
+//        find  the missing element in arrays
+        int[] arr ={1,2,5,6,7,8,10};
+        int  a = 1;
+         int res;
+         for(int i=0;i<arr.length;i++){
+             if(arr[i]!=a){
+                 res=a;
+        System.out.println(res);
+                 return;
+             }
+                  a++;
+         }
+    }
+}
